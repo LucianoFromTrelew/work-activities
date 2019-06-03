071fffbf897c78a07b0a337b2f3d59904ab7e45e
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-03 01:08:24 -0300

    Add more auth routes

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Response, Request, NextFunction } from "express";
 import User from "../models/user.model";
-import { hashPassword } from "../utils";
 
 const login = async (
   request: Request,
@@ -14,8 +13,34 @@ const login = async (
     await user.save();
     response.status(200).send({ apiToken });
   } catch (err) {
-    response.status(400).send();
+    response.status(400).send({ msg: "Bad request" });
   }
 };
 
-export { login };
+const logout = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const { authorization } = request.headers;
+    const apiToken = authorization.split(" ")[1];
+    const user = await User.findOne({ apiToken });
+    user.clearApiToken();
+    await user.save();
+    response.status(200).send();
+  } catch (err) {
+    response.status(400).send({ msg: "Bad request" });
+  }
+};
+
+const isAuthenticated = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const { username, apiToken } = request.user;
+  response.status(200).send({ username, apiToken });
+};
+
+export { login, logout, isAuthenticated };
--- server/src/router/auth.ts
+++ server/src/router/auth.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { login } from "../controllers/auth.controller";
+import { login, logout, isAuthenticated } from "../controllers/auth.controller";
+import passport from "../config/passport";
 
 const router = Router();
 
 router.post("/login", login);
+router.post("/logout", logout);
+router.get(
+  "/isauthenticated",
+  passport.authenticate("bearer", { session: false }),
+  isAuthenticated
+);
 
 export default router;
