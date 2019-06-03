e9f77bd488535b0d880079a9a236244d989cbcb9
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-03 00:01:44 -0300

    Update User model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,12 +1,34 @@
-import { Typegoose, prop, instanceMethod, staticMethod } from "typegoose";
+import { Typegoose, prop, instanceMethod, staticMethod, pre } from "typegoose";
+import { hashPassword } from "../utils";
+import { generate } from "randomstring";
 
+@pre<UserClass>("save", function(next) {
+  this.password = hashPassword(this.password);
+  next();
+})
 class UserClass extends Typegoose {
   @prop({ required: true, unique: true })
-  username: String;
+  username: string;
   @prop({ required: true })
-  password: String;
+  password: string;
   @prop()
-  apiToken: String;
+  apiToken: string;
+
+  @instanceMethod
+  generateApiToken(): string {
+    this.apiToken = generate();
+    return this.apiToken;
+  }
+
+  @staticMethod
+  static async getUserByUsernameAndPassword(
+    username: string,
+    clearPassword: string
+  ) {
+    const password = hashPassword(clearPassword);
+    const user = await User.findOne({ username, password });
+    return user;
+  }
 }
 
 const User = new UserClass().getModelForClass(UserClass);
