8b2e97e9d097167d1333ac2b65c6ad9af0cc685a
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-03 09:50:47 -0300

    Fix app.test.ts
    Was creating an user and not deleting it afterwards

--- server/src/__test__/app.test.ts
+++ server/src/__test__/app.test.ts
@@ -5,21 +5,30 @@ import User from "../models/user.model";
 
 describe("app", () => {
   let app: Application;
+  const mongoDb = process.env.MONGO_DB;
 
   beforeEach(async () => {
+    process.env.MONGO_DB = "test";
     app = await getApp();
   });
+
+  afterEach(async () => {
+    process.env.MONGO_DB = mongoDb;
+    await User.deleteMany({});
+  });
+
   it("should work", () => {
     expect(1).toBe(1);
   });
-  it("works correctly on /", async () => {
+
+  it("returns 401 on / if not authenticated", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(401);
   });
-  it("now works", async () => {
-    await User.deleteMany({});
-    const username = "username";
-    const password = "password";
+
+  it("works correctly on / if authenticated", async () => {
+    const username = "testusername";
+    const password = "testpassword";
     const user = new User({ username, password });
     user.generateApiToken();
     await user.save();
