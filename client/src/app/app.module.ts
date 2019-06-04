65d9574976bca5529a7ee45a6675c9e6e1c696ab
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-04 13:33:11 -0300

    Add ActivityDetail component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppHeaderComponent } from "./layout/app-header/app-header.component";
 import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityDetailComponent } from './activity-detail/activity-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, AppHeaderComponent, ActivityListComponent],
+  declarations: [AppComponent, AppHeaderComponent, ActivityListComponent, ActivityDetailComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
