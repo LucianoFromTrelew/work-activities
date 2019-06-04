7421e1af9a293de36fa182d88380a945a840d12d
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-04 12:18:04 -0300

    Generate ActivityList component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppHeaderComponent } from "./layout/app-header/app-header.component";
+import { ActivityListComponent } from './activity-list/activity-list.component';
 
 @NgModule({
-  declarations: [AppComponent, AppHeaderComponent],
+  declarations: [AppComponent, AppHeaderComponent, ActivityListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
