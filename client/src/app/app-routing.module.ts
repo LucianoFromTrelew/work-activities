2f653855b77144d8535d36b58455f4131ae8c4c3
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-04 13:26:48 -0300

    WIP

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ActivityListComponent } from "./activity-list/activity-list.component";
+import { ActivityDetailComponent } from "./activity-detail/activity-detail.component";
 
 const routes: Routes = [
   { path: "activity", component: ActivityListComponent },
   { path: "", redirectTo: "/activity", pathMatch: "full" },
+  { path: "activity/:activityId", component: ActivityDetailComponent },
   { path: "**", redirectTo: "/activity" }
 ];
 
