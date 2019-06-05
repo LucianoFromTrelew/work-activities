cf36e6f6e9f8041f95cffae3285899906c144f85
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-05 00:36:08 -0300

    Update app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivitiesService } from "./activities.service";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = "client";
 
-  constructor(private activitiesService: ActivitiesService) {}
-  ngOnInit() {
-    this.activitiesService.getActivities().subscribe(activities => {
-      console.log(activities);
-    });
-  }
+  constructor() {}
 }
