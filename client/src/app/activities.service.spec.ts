1b5278313723095c51b05e0fd44188fad2b64b0a
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-05 01:48:48 -0300

    Update tests

--- client/src/app/activities.service.spec.ts
+++ client/src/app/activities.service.spec.ts
@@ -1,13 +1,26 @@
 import { ActivitiesService } from "./activities.service";
-import { asyncData } from "./utils/testing";
+import { fakeAsync, flush, tick } from "@angular/core/testing";
 
 describe("ActivitiesService", () => {
-  let httpClientStub: { get: any };
+  let httpClientStub;
   let service: ActivitiesService;
+  const expectedData = [
+    {
+      id: 1,
+      title: "some title",
+      description: "some description",
+      tags: ["tags"],
+      geolocation: { latitude: 12, longitude: 21 }
+    }
+  ];
 
   beforeEach(() => {
-    const get = jasmine.createSpyObj("get", ["toPromise"]);
-    httpClientStub.get = get;
+    httpClientStub = jasmine.createSpyObj("HttpClient", ["get"]);
+    httpClientStub.get = jasmine.createSpy().and.returnValue({
+      toPromise: jasmine
+        .createSpy()
+        .and.returnValue(new Promise(resolve => expectedData))
+    });
     service = new ActivitiesService(httpClientStub as any);
   });
 
@@ -15,16 +28,23 @@ describe("ActivitiesService", () => {
     expect(service).toBeTruthy();
   });
 
-  it("should return correct data", () => {
-    const expectedData: any = [
-      { id: 1, name: "coso" },
-      { id: 2, name: "otro" }
-    ];
-    httpClientStub.get.toPromise.and.returnValue(asyncData(expectedData));
+  it("should return correct data", fakeAsync(() => {
     service.getActivities().then(data => {
       expect(data).toEqual(expectedData, "expected data"), fail;
     });
+    expect(httpClientStub.get.calls.count()).toBe(1, "one call");
+  }));
 
-    expect(httpClientStub.get.toPromise.calls.count()).toBe(1, "one call");
-  });
+  it("should not make a second http call if it already has activities", fakeAsync(() => {
+    service
+      .getActivities()
+      .then(data => service.getActivities())
+      .then(data => {});
+
+    flush();
+    expect(httpClientStub.get.calls.count()).toBeLessThan(
+      2,
+      "no more than one call"
+    );
+  }));
 });
