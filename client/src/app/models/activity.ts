832ff8a3c0dd1313daf17fddf99e621864fb294f
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-05 00:34:42 -0300

    Update activity model class

--- client/src/app/models/activity.ts
+++ client/src/app/models/activity.ts
@@ -1,2 +1,9 @@
 export class Activity {
+  constructor(
+    public id: number,
+    public title: string,
+    public description: string,
+    public tags: string[],
+    public geolocation: { latitude: number; longitude: number }
+  ) {}
 }
