e965be4bb0c25d52ecd43c23fde6adf0165e721e
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-05 01:47:50 -0300

    Update activity model class tests

--- client/src/app/models/activity.spec.ts
+++ client/src/app/models/activity.spec.ts
@@ -1,7 +1,12 @@
-import { Activity } from './activity';
+import { Activity } from "./activity";
 
-describe('Activity', () => {
-  it('should create an instance', () => {
-    expect(new Activity()).toBeTruthy();
+describe("Activity", () => {
+  it("should create an instance", () => {
+    expect(
+      new Activity(1, "some title", "some description", ["tags"], {
+        latitude: 12,
+        longitude: 21
+      })
+    ).toBeTruthy();
   });
 });
