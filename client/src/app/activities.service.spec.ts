2a7cd6bbbec4d560fea3746149930e6b0baed46e
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-05 16:56:59 -0300

    Update service tests

--- client/src/app/activities.service.spec.ts
+++ client/src/app/activities.service.spec.ts
@@ -1,50 +1,108 @@
 import { ActivitiesService } from "./activities.service";
-import { fakeAsync, flush, tick } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
+import { fakeAsync, flush, tick, TestBed, inject } from "@angular/core/testing";
+import { HttpClient } from "@angular/common/http";
 
+const expectedData = [
+  {
+    id: 1,
+    title: "some title",
+    description: "some description",
+    tags: ["tags"],
+    geolocation: { latitude: 12, longitude: 21 }
+  },
+  {
+    id: 2,
+    title: "some other title",
+    description: "some other description",
+    tags: ["other", "tags"],
+    geolocation: { latitude: 12, longitude: 21 }
+  }
+];
 describe("ActivitiesService", () => {
-  let httpClientStub;
   let service: ActivitiesService;
-  const expectedData = [
-    {
-      id: 1,
-      title: "some title",
-      description: "some description",
-      tags: ["tags"],
-      geolocation: { latitude: 12, longitude: 21 }
-    }
-  ];
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    httpClientStub = jasmine.createSpyObj("HttpClient", ["get"]);
-    httpClientStub.get = jasmine.createSpy().and.returnValue({
-      toPromise: jasmine
-        .createSpy()
-        .and.returnValue(new Promise(resolve => expectedData))
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ActivitiesService]
     });
-    service = new ActivitiesService(httpClientStub as any);
+
+    service = TestBed.get(ActivitiesService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
-  it("should be created", () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it("should be initialized", () => {
     expect(service).toBeTruthy();
   });
 
-  it("should return correct data", fakeAsync(() => {
-    service.getActivities().then(data => {
-      expect(data).toEqual(expectedData, "expected data"), fail;
+  it("should return correct data", () => {
+    service.getActivities().then(activities => {
+      expect(activities).toBe(expectedData);
     });
-    expect(httpClientStub.get.calls.count()).toBe(1, "one call");
-  }));
 
-  it("should not make a second http call if it already has activities", fakeAsync(() => {
+    const requestWrapper = httpMock.expectOne(
+      "http://localhost:5000/api/activity"
+    );
+
+    expect(requestWrapper.request.method).toBe("GET");
+
+    requestWrapper.flush(expectedData);
+  });
+
+  it("should return correctly an activity by id", () => {
+    const id = 1;
+
+    service.getActivityById(id).then(activity => {
+      expect(activity).toEqual(expectedData[0]);
+    });
+
+    const requestWrapper = httpMock.expectOne(
+      `http://localhost:5000/api/activity/${id}`
+    );
+
+    expect(requestWrapper.request.method).toBe("GET");
+
+    requestWrapper.flush(expectedData[0]);
+  });
+});
+
+describe("ActivitiesService 2", () => {
+  let service;
+  let spy;
+
+  beforeEach(() => {
+    spy = jasmine.createSpyObj("HttpClient mock", ["get"]);
+    spy.get = jasmine.createSpy().and.returnValue({
+      toPromise: jasmine
+        .createSpy()
+        .and.returnValue(new Promise(resolve => resolve(expectedData)))
+    });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ActivitiesService, { provide: HttpClient, useValue: spy }]
+    });
+
+    service = TestBed.get(ActivitiesService);
+  });
+
+  it("should only be making one http request on getActivities", fakeAsync(() => {
     service
       .getActivities()
-      .then(data => service.getActivities())
-      .then(data => {});
+      .then(_ => {
+        return service.getActivities();
+      })
+      .then(_ => {});
 
-    flush();
-    expect(httpClientStub.get.calls.count()).toBeLessThan(
-      2,
-      "no more than one call"
-    );
+    tick();
+    expect(spy.get.calls.count()).toBeLessThan(2);
   }));
 });
