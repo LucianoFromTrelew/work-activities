106492117f7cd10d368bf183d534d4244cefbedb
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-06 10:21:12 -0300

    Add activity-card component

--- client/src/app/components/activity-card/activity-card.component.spec.ts
+++ client/src/app/components/activity-card/activity-card.component.spec.ts
@@ -1,25 +1,76 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+  flush
+} from "@angular/core/testing";
 
-import { ActivityCardComponent } from './activity-card.component';
+import { ActivityCardComponent } from "./activity-card.component";
+import { MatCardModule, MatChipsModule } from "@angular/material";
+import { getFakeActivities } from "src/app/pages/utils/testing";
+import { Activity } from "src/app/models/activity";
+import { By } from "@angular/platform-browser";
+import { CdkHeaderRowDef } from "@angular/cdk/table";
+import { DebugElement } from "@angular/core";
 
-describe('ActivityCardComponent', () => {
+describe("ActivityCardComponent", () => {
   let component: ActivityCardComponent;
   let fixture: ComponentFixture<ActivityCardComponent>;
+  let activityCardDe: DebugElement;
+  let deleteBtnDe: DebugElement;
+  const fakeActivity = getFakeActivities()[0];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActivityCardComponent ]
-    })
-    .compileComponents();
+      declarations: [ActivityCardComponent],
+      imports: [MatCardModule, MatChipsModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ActivityCardComponent);
     component = fixture.componentInstance;
+    component.activity = fakeActivity;
+    activityCardDe = fixture.debugElement.query(
+      By.css("[data-testid='activity-card']")
+    );
+    deleteBtnDe = fixture.debugElement.query(
+      By.css("[data-testid='delete-btn']")
+    );
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should call 'onCardClick' when clicking the card", () => {
+    const onCardClickSpy = jasmine.createSpy("onCardClick");
+    component.onCardClick = onCardClickSpy;
+    activityCardDe.triggerEventHandler("click", null);
+    expect(onCardClickSpy).toHaveBeenCalled();
+  });
+
+  it("should emit 'selected' event on card click", () => {
+    let expectedActivity: Activity;
+    component.selected.subscribe(activity => (expectedActivity = activity));
+    activityCardDe.triggerEventHandler("click", null);
+    expect(expectedActivity).toEqual(fakeActivity);
+  });
+
+  it("should call 'onDeleteClick' when click the delete button", () => {
+    const onDeleteClickSpy = jasmine.createSpy("onDeleteClickSpy");
+    component.onDeleteClick = onDeleteClickSpy;
+    deleteBtnDe.triggerEventHandler("click", null);
+    expect(onDeleteClickSpy).toHaveBeenCalled();
+  });
+
+  it("should emit 'delete' event on delete button click", () => {
+    let expectedActivity: Activity;
+    component.delete.subscribe(activity => (expectedActivity = activity));
+    deleteBtnDe.triggerEventHandler("click", null);
+    expect(expectedActivity).toEqual(fakeActivity);
   });
 });
--- client/src/app/components/activity-card/activity-card.component.ts
+++ client/src/app/components/activity-card/activity-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Activity } from "../../models/activity";
 
 @Component({
-  selector: 'app-activity-card',
-  templateUrl: './activity-card.component.html',
-  styleUrls: ['./activity-card.component.css']
+  selector: "app-activity-card",
+  templateUrl: "./activity-card.component.html",
+  styleUrls: ["./activity-card.component.css"]
 })
 export class ActivityCardComponent implements OnInit {
+  @Input() activity: Activity;
+  @Output() selected: EventEmitter<Activity> = new EventEmitter<Activity>();
+  @Output() delete: EventEmitter<Activity> = new EventEmitter<Activity>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onCardClick() {
+    this.selected.emit(this.activity);
   }
 
+  onDeleteClick() {
+    this.delete.emit(this.activity);
+  }
 }
