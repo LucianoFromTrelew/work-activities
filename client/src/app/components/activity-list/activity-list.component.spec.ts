cf9e5d4f40b3fe7019a008c00858cf650c9dc23d
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-06 11:16:17 -0300

    Update activity-list component

--- client/src/app/components/activity-list/activity-list.component.spec.ts
+++ client/src/app/components/activity-list/activity-list.component.spec.ts
@@ -1,25 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ActivityListComponent } from './activity-list.component';
+import { ActivityListComponent } from "./activity-list.component";
+import { ActivityCardComponent } from "../activity-card/activity-card.component";
+import { MatCardModule, MatChipsModule } from "@angular/material";
+import { getFakeActivities } from "src/app/pages/utils/testing";
+import { Activity } from "src/app/models/activity";
 
-describe('ActivityListComponent', () => {
+describe("ActivityListComponent", () => {
   let component: ActivityListComponent;
   let fixture: ComponentFixture<ActivityListComponent>;
+  const data = getFakeActivities(3);
+  let activityCard: ActivityCardComponent;
+  let selectedActivity: Activity;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActivityListComponent ]
-    })
-    .compileComponents();
+      declarations: [ActivityListComponent, ActivityCardComponent],
+      imports: [MatCardModule, MatChipsModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ActivityListComponent);
     component = fixture.componentInstance;
+    component.activities = data;
     fixture.detectChanges();
+    activityCard = component.activityCards.toArray()[0];
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    activityCard = null;
+    selectedActivity = null;
+  });
+
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should render correctly", () => {
+    expect(
+      fixture.nativeElement.querySelectorAll(".activity-card").length
+    ).toBe(data.length);
+  });
+
+  it("should emit 'selected' event on some card click", () => {
+    component.selected.subscribe(activity => (selectedActivity = activity));
+    activityCard.onCardClick();
+    expect(selectedActivity).toEqual(activityCard.activity);
   });
 });
--- client/src/app/components/activity-list/activity-list.component.ts
+++ client/src/app/components/activity-list/activity-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChildren,
+  AfterContentChecked
+} from "@angular/core";
+import { Activity } from "../../models/activity";
 
 @Component({
-  selector: 'app-activity-list',
-  templateUrl: './activity-list.component.html',
-  styleUrls: ['./activity-list.component.css']
+  selector: "app-activity-list",
+  templateUrl: "./activity-list.component.html",
+  styleUrls: ["./activity-list.component.css"]
 })
 export class ActivityListComponent implements OnInit {
+  @ViewChildren("activityCard") activityCards;
+  @Input() activities: Activity[];
+  @Output() selected: EventEmitter<Activity> = new EventEmitter<Activity>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSelectedActivity(activity: Activity) {
+    this.selected.emit(activity);
   }
-
 }
