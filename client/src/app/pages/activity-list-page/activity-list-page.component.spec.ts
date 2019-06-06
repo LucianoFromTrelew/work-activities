4a952e25dd82fe5a04900942046a4738349f0683
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-06 19:58:50 -0300

    Update activity list page

--- client/src/app/pages/activity-list-page/activity-list-page.component.spec.ts
+++ client/src/app/pages/activity-list-page/activity-list-page.component.spec.ts
@@ -1,25 +1,89 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick
+} from "@angular/core/testing";
 
-import { ActivityListPageComponent } from './activity-list-page.component';
+import { ActivityListPageComponent } from "./activity-list-page.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+  MatProgressSpinnerModule,
+  MatCardModule,
+  MatChipsModule
+} from "@angular/material";
+import { ActivityListComponent } from "src/app/components/activity-list/activity-list.component";
+import { ActivitiesService } from "src/app/activities.service";
+import { ActivityCardComponent } from "src/app/components/activity-card/activity-card.component";
+import { getFakeActivities } from "../../utils/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
-describe('ActivityListPageComponent', () => {
+describe("ActivityListPageComponent", () => {
+  const data = getFakeActivities(3);
   let component: ActivityListPageComponent;
   let fixture: ComponentFixture<ActivityListPageComponent>;
 
   beforeEach(async(() => {
+    const activitiesServiceMock = jasmine.createSpyObj("ActivitiesService", [
+      "getActivities"
+    ]);
+    activitiesServiceMock.getActivities.and.returnValue(
+      new Promise(resolve => resolve(data))
+    );
     TestBed.configureTestingModule({
-      declarations: [ ActivityListPageComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ActivityListPageComponent,
+        ActivityListComponent,
+        ActivityCardComponent
+      ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatProgressSpinnerModule,
+        MatCardModule,
+        MatChipsModule
+      ],
+      providers: [
+        { provide: ActivitiesService, useValue: activitiesServiceMock }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ActivityListPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("shoud render an spinner while fetching data", fakeAsync(() => {
+    expect(
+      fixture.nativeElement.querySelector("[data-testid='spinner']")
+    ).toBeDefined();
+    fixture.detectChanges(); // onInit()
+    tick();
+    fixture.detectChanges(); // has activities - remove spinner
+    expect(
+      fixture.nativeElement.querySelector("[data-testid='spinner']")
+    ).toBeNull();
+  }));
+
+  it("should get activities", fakeAsync(() => {
+    expect(component.hasActivities()).toBeFalsy();
+    fixture.detectChanges(); // onInit()
+    tick();
+    expect(component.hasActivities()).toBeTruthy();
+  }));
+
+  it("should render as many cards as activities", fakeAsync(() => {
+    fixture.detectChanges(); // onInit()
+    tick();
+    fixture.detectChanges(); // update UI
+    expect(
+      fixture.nativeElement.querySelectorAll(".activity-card").length
+    ).toBe(data.length);
+  }));
 });
--- client/src/app/pages/activity-list-page/activity-list-page.component.ts
+++ client/src/app/pages/activity-list-page/activity-list-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivitiesService } from "src/app/activities.service";
+import { Activity } from "src/app/models/activity";
+import { Router, NavigationEnd, Event } from "@angular/router";
 
 @Component({
-  selector: 'app-activity-list-page',
-  templateUrl: './activity-list-page.component.html',
-  styleUrls: ['./activity-list-page.component.css']
+  selector: "app-activity-list-page",
+  templateUrl: "./activity-list-page.component.html",
+  styleUrls: ["./activity-list-page.component.css"]
 })
 export class ActivityListPageComponent implements OnInit {
+  private activities: Activity[] = [];
+  private fetchingData: boolean = true;
+  private error: boolean = false;
+  constructor(
+    private activitiesService: ActivitiesService,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      this.activities = await this.activitiesService.getActivities();
+    } catch (error) {
+      this.error = true;
+    } finally {
+      this.fetchingData = false;
+    }
   }
 
+  hasActivities() {
+    return !this.fetchingData;
+  }
+
+  onSelectedActivity(activity: Activity) {
+    this.router.navigate(["/activity", activity.id]);
+  }
 }
