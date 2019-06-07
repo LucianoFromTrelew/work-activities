81c32e9a25969650e4f308cb31b017ab9e56565e
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-06 22:56:57 -0300

    Add login page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ActivityListPageComponent } from "./pages/activity-list-page/activity-list-page.component";
 import { ActivityDetailPageComponent } from "./pages/activity-detail-page/activity-detail-page.component";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/activity", pathMatch: "full" },
   { path: "activity", component: ActivityListPageComponent },
   { path: "activity/:activityId", component: ActivityDetailPageComponent },
+  { path: "login", component: LoginPageComponent },
   { path: "**", redirectTo: "/activity" }
 ];
 
