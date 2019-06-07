1b21dd0b206475a9c08d5d9941add00c3feecb11
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-07 09:39:37 -0300

    Update login page

--- client/src/app/pages/login-page/login-page.component.spec.ts
+++ client/src/app/pages/login-page/login-page.component.spec.ts
@@ -1,16 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LoginPageComponent } from './login-page.component';
+import { LoginPageComponent } from "./login-page.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import {
+  MatProgressSpinnerModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatInputModule,
+  MatSnackBarModule
+} from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AlertService } from "src/app/alert.service";
 
-describe('LoginPageComponent', () => {
+describe("LoginPageComponent", () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatProgressSpinnerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSnackBarModule,
+        MatCardModule
+      ],
+      declarations: [LoginPageComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +41,7 @@ describe('LoginPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- client/src/app/pages/login-page/login-page.component.ts
+++ client/src/app/pages/login-page/login-page.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormControl } from "@angular/forms";
+import { AuthService } from "src/app/auth.service";
+import { Router } from "@angular/router";
+import { AlertService } from "src/app/alert.service";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.css"]
 })
 export class LoginPageComponent implements OnInit {
+  private username = new FormControl("", [Validators.required]);
+  private password = new FormControl("", [Validators.required]);
 
-  constructor() { }
+  private submitted: boolean = false;
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alert: AlertService
+  ) {}
+
+  async onSubmit() {
+    if (this.isFormValid()) return;
+    this.submitted = true;
+    try {
+      const res = await this.authService
+        .login(this.username.value, this.password.value)
+        .toPromise();
+      this.router.navigate(["/"]);
+    } catch (error) {
+      this.alert.error("Ha ocurrido un error");
+    } finally {
+      this.submitted = false;
+      this.username.reset();
+      this.password.reset();
+    }
   }
 
+  getUsernameErrorMessage() {
+    return this.username.hasError("required")
+      ? "Ingrese su nombre de usuario"
+      : "";
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError("required") ? "Ingrese su contraseña" : "";
+  }
+
+  isFormValid() {
+    return this.username.invalid || this.password.invalid;
+  }
+
+  ngOnInit() {}
 }
