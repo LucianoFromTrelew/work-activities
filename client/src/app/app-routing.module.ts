23ae696b2fde897919051b80e0dedeb053d2757c
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-06-07 13:57:08 -0300

    Add edit route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ActivityListPageComponent } from "./pages/activity-list-page/activity-l
 import { ActivityDetailPageComponent } from "./pages/activity-detail-page/activity-detail-page.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { ActivityCreatePageComponent } from "./pages/activity-create-page/activity-create-page.component";
+import { ActivityEditPageComponent } from "./pages/activity-edit-page/activity-edit-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/activity", pathMatch: "full" },
   { path: "activity", component: ActivityListPageComponent },
   { path: "activity/create", component: ActivityCreatePageComponent },
   { path: "activity/:activityId", component: ActivityDetailPageComponent },
+  { path: "activity/:activityId/edit", component: ActivityEditPageComponent },
   { path: "login", component: LoginPageComponent },
   { path: "**", redirectTo: "/activity" }
 ];
